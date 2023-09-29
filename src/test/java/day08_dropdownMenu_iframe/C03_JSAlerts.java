a17e505ee591d14004b32e6eeeff95fc9d849de4
ertugrulkececii <ertgrlkcc@gmail.com>
2023-09-29 22:33:04 +0200

    ders sonu

---
+++ src/test/java/day08_dropdownMenu_iframe/C03_JSAlerts.java
@@ -0,0 +1,12 @@
+package day08_dropdownMenu_iframe;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+public class C03_JSAlerts extends TestBase {
+
+
+}
